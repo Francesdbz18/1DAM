64fb994e9ae246eabfe79a4a51494e31a487631e
Francesco <francesdbz18pc@outlook.es>
2023-12-05 09:41:41 +0100

    Creado nuevo proyecto Tema3Arrays

---
+++ tema3arrays/src/main/java/tema3arrays/Funciones.java
@@ -0,0 +1,98 @@
+package tema3arrays;
+
+public class Funciones {
+    public static int[] bubblesortDesc(int[] numbers) {
+        boolean swapped = true;
+        int temp;
+        for (int i = numbers.length-1; i>0 && swapped; i--) {
+            swapped = false;
+            for (int j = 0; j<i; j++) {  //cambiar a j > i para versión ascendiente
+                if (numbers[j] < numbers[j+1]) {
+                temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers [j+1] = temp;
+                swapped = true;
+                }
+            }
+        }
+        return numbers;
+    }
+    public static int[] bubblesortAsc(int[] numbers) {
+        boolean swapped = true;
+        int temp;
+        for (int i = numbers.length-1; i>0 && swapped; i--) {
+            swapped = false;
+            for (int j = 0; j>i; j++) {  //cambiar a j > i para versión ascendiente
+                if (numbers[j] < numbers[j+1]) {
+                temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers [j+1] = temp;
+                swapped = true;
+                }
+            }
+        }
+        return numbers;
+    }
+    public static int [] bubblesort2Asc (int[] numbers) {
+        int i = 0;
+        int j = numbers.length-1;
+        int temp;
+        boolean swapped = true;
+        while(i>j &&swapped) { //cambiar a i<j para versión descendiente
+            swapped = false;
+            for (int k = i; k<j; k++) {
+                if(numbers[k]>numbers[k+1]) {
+                    temp = numbers[k];
+                    numbers [k] = numbers [k+1];
+                    numbers [k+1] = temp;
+                    swapped = true;
+                }//fin if
+            }//fin for
+            j--;
+            if(swapped) {
+                swapped = false;
+                for (int k = j; k>i; k--) {
+                    if(numbers[k] < numbers [k-1]) {
+                        temp = numbers[k];
+                        numbers [k] = numbers [k-1];
+                        numbers [k-1] = temp;
+                        swapped = true;
+                    }//fin if
+                } //fin for
+            }//fin if
+            i++;
+        }//fin while
+        return numbers;
+    }
+    public static int [] bubblesort2Desc (int[] numbers) {
+        int i = 0;
+        int j = numbers.length-1;
+        int temp;
+        boolean swapped = true;
+        while(i<j &&swapped) { //cambiar a i<j para versión descendiente
+            swapped = false;
+            for (int k = i; k<j; k++) {
+                if(numbers[k]>numbers[k+1]) {
+                    temp = numbers[k];
+                    numbers [k] = numbers [k+1];
+                    numbers [k+1] = temp;
+                    swapped = true;
+                }//fin if
+            }//fin for
+            j--;
+            if(swapped) {
+                swapped = false;
+                for (int k = j; k>i; k--) {
+                    if(numbers[k] < numbers [k-1]) {
+                        temp = numbers[k];
+                        numbers [k] = numbers [k-1];
+                        numbers [k-1] = temp;
+                        swapped = true;
+                    }//fin if
+                } //fin for
+            }//fin if
+            i++;
+        }//fin while
+        return numbers;
+    }
+}
---
+++ tema3arrays/src/main/java/tema3arrays/Main.java
@@ -0,0 +1,8 @@
+package tema3arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = {10, 1, 5, 40, 12, 34, 44, 12, 11, 91};
+        Funciones.bubblesort2Asc(array);
+    }
+}
