4dda134d4927a2d809ac48a2ce5873f7811406de
Francesco <francesdbz18pc@outlook.es>
2023-12-05 09:26:07 +0100

    Bubble sort y chistesito

---
+++ clase/src/main/java/actividades/Actividad3_2Burbuja.java
@@ -0,0 +1,73 @@
+package actividades;
+
+public class Actividad3_2Burbuja {
+    public static int[] bubblesort(int[] numbers) {
+        boolean swapped = true;
+        int temp;
+        for (int i = numbers.length-1; i>0 && swapped; i--) {
+            swapped = false;
+            for (int j = 0; j<i; j++) {  //cambiar a j > i para versión ascendiente
+                if (numbers[j] < numbers[j+1]) {
+                temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers [j+1] = temp;
+                swapped = true;
+                }
+            }
+        }
+        return numbers;
+    }
+    public static int [] bubblesort2 (int[] numbers) {
+        int i = 0;
+        int j = numbers.length-1;
+        int temp;
+        boolean swapped = true;
+        while(i>j &&swapped) { //cambiar a i<j para versión descendiente
+            swapped = false;
+            for (int k = i; k<j; k++) {
+                if(numbers[k]>numbers[k+1]) {
+                    temp = numbers[k];
+                    numbers [k] = numbers [k+1];
+                    numbers [k+1] = temp;
+                    swapped = true;
+                }//fin if
+            }//fin for
+            j--;
+            if(swapped) {
+                swapped = false;
+                for (int k = j; k>i; k--) {
+                    if(numbers[k] < numbers [k-1]) {
+                        temp = numbers[k];
+                        numbers [k] = numbers [k-1];
+                        numbers [k-1] = temp;
+                        swapped = true;
+                    }//fin if
+                } //fin for
+            }//fin if
+            i++;
+        }//fin while
+        return numbers;
+    }
+    static void imprimirArrayUni(int a[], int length) {
+        System.out.print("El resultado del array es: [");
+        for (int i = 0; i<length;i++) {
+            System.out.print(a[i]);
+            if (i<length-1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+    public static void main(String[] args) {
+        int [] array = {10, 1, 5, 40, 12, 34, 44, 12, 11, 91};
+        int longitud = array.length;
+
+        int [] arrayDesc 
+
+        bubblesort(array);
+        imprimirArrayUni(array, longitud);
+        System.out.println("\nVersión mejorada: ");
+        bubblesort2(array);
+        imprimirArrayUni(array, longitud);
+    }
+}
---
+++ clase/src/main/java/actividades/profe.java
@@ -0,0 +1,8 @@
+package actividades;
+
+public class profe {
+    public static void main(String[] args) {
+        String jaime = "me la sopla sosio jajaxd";
+        System.out.println("Jaime dice: "+jaime);
+    }
+}
