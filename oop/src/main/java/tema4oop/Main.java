9ff16db1d6fc1028342f5c62d6da277bcfd54b38
Francesco <francesdbz18pc@outlook.es>
2023-12-19 09:14:33 +0100

    oop

---
+++ oop/src/main/java/tema4oop/Main.java
@@ -0,0 +1,9 @@
+package tema4oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Punto punto1, punto2, punto3 = new Punto();
+        Punto punto4 = new Punto(5, 2);
+        punto1 = punto4;
+    }
+}
