a4539fe8ad81868321c495f47bb9b9e453fd93ab
Francesco <francesdbz18pc@outlook.es>
2023-12-19 09:09:19 +0100

    oop

---
+++ oop/src/main/java/tema4oop/Punto.java
@@ -0,0 +1,37 @@
+package tema4oop;
+
+public class Punto {
+    private int x, y;
+
+    /*CONSTRUCTORS*/
+    public Punto (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Punto() {}
+
+    /*GETTERS & SETTERS*/
+    public void setX (int x) {
+
+    }
+    public void setY (int y) {
+        
+    }
+    public int getX () {
+        return x;
+    }
+    public int getY () {
+        return y;
+    }
+
+    public double modulo() {
+        double modulo = 0;
+        return modulo;
+    }
+    public double fase () {
+        double fase = 0;
+        return fase;
+    }
+
+}
