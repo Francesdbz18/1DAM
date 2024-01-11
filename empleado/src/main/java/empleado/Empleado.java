14a015384593de8bd723f373cedf903c62ea5a15
Francesco <francesdbz18pc@outlook.es>
2024-01-11 13:29:44 +0100

    AE

---
+++ empleado/src/main/java/empleado/Empleado.java
@@ -0,0 +1,15 @@
+package empleado;
+
+public class Empleado {
+    private String nombre;
+    private double salario;
+
+    public Empleado () {
+
+    }
+
+    public Empleado (String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+}
