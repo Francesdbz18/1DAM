db86bdfa3f0a40c1bce63a9f1dc52b7be73662a2
Francesco <francesdbz18pc@outlook.es>
2024-01-11 12:28:06 +0100

    WAOS

--- individuo/src/main/java/tareaindividuo/Individuo.java
+++ individuo/src/main/java/tareaindividuo/Individuo.java
@@ -5,39 +5,60 @@ public class Individuo { //TODO: edad, altura
     private String nombre;
     private int fechaNacimiento;
     private static int poblacion;
+    private static int edad;
+    private double altura;
 
-    public Individuo (String dni, String nombre, int fechaNacimiento) {
+    public Individuo (String dni, String nombre, int fechaNacimiento, int anyo, double altura) {
         super();
         this.dni = dni;
         this.nombre = nombre;
         this.fechaNacimiento = fechaNacimiento;
+        this.altura = altura;
         poblacion++;
+        edad = anyo - fechaNacimiento;
     }
     public String getDni() {
         return dni;
     }
-    public void setDni() {
+    public void setDni(String dni) {
         this.dni = dni;
     }
 
     public String getNombre() {
         return nombre;
     }
-    public void setNombre() {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     public int getFechaNacimiento() {
         return fechaNacimiento;
     }
-    public void setFechaNacimiento() {
+    public void setFechaNacimiento(int fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
 
     public static int getPoblacion () {
         return poblacion;
     }
-    public static void setPoblacion() {
+    public static void setPoblacion(int poblacion) {
         Individuo.poblacion = poblacion;
     }
+
+    public int getEdad () {
+        return edad;
+    }
+
+    public static void setEdad(int edad) {
+        Individuo.edad = edad;
+    }
+
+    public double getAltura () {
+        return altura;
+    }
+    public void setAltura (double altura) {
+        this.altura = altura;
+
+    }
+
 }
--- individuo/src/main/java/tareaindividuo/Main.java
+++ individuo/src/main/java/tareaindividuo/Main.java
@@ -2,10 +2,10 @@ package tareaindividuo;
 
 public class Main {
     public static void main(String[] args) {
-        Individuo alumno1 = new Individuo("111111", "Juanito Alimaña", 1975);
-        Individuo alumno2 = new Individuo("111011", "Pedro Navaja", 1945);
-        Individuo alumno3 = new Individuo("111111", "Madame Kalalú", 1985);
-        Individuo alumno4 = new Individuo("111111", "Ligia Elena", 1969);
+        Individuo alumno1 = new Individuo("111111", "Juanito Alimaña", 1975, 2024);
+        Individuo alumno2 = new Individuo("111011", "Pedro Navaja", 1945, 2024);
+        Individuo alumno3 = new Individuo("111111", "Madame Kalalú", 1985, 2024);
+        Individuo alumno4 = new Individuo("111111", "Ligia Elena", 1969, 2024);
         System.out.println("Población: "+Individuo.getPoblacion());
     }
 }
