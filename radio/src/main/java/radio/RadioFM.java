833af8cb8b642ecbcbacb7833dc349af79c90b83
Francesco <francesdbz18pc@outlook.es>
2024-01-12 09:26:06 +0100

    Radio

--- radio/src/main/java/radio/RadioFM.java
+++ radio/src/main/java/radio/RadioFM.java
@@ -1,5 +1,27 @@
 package radio;
 
 public class RadioFM {
-    
+    private final static int frecMin = 80;
+    private final static int frecMax = 105;
+    private float frecuencia;
+    private final static float var = 0.5f;
+
+    public RadioFM (float frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    public float getFrecuencia () {
+        return frecuencia;
+    }
+    public void setFrecuencia (float frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    public static int getfrecMin () {
+        return frecMin;
+    }
+
+    public static int getfrecMax () {
+        return frecMax;
+    }
 }
