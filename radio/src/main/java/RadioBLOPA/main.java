b640fcf2995ee2c638b06eeb36f4908d6a7729ea
Francesco <francesdbz18pc@outlook.es>
2024-01-15 11:09:27 +0100

    radiojabor y RadioBLOPA

---
+++ radio/src/main/java/RadioBLOPA/main.java
@@ -0,0 +1,49 @@
+package Radio;
+
+import java.util.Scanner;
+
+public class main {
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		Radio radioFM = new Radio();
+		
+		System.out.println("Introduzca la frecuencia deseada: ");
+		double frecuenciaInicial = scanner.nextDouble();
+		radioFM.setFrecuencia(frecuenciaInicial);
+		int tipo = 0;
+		
+		/*System.out.println("¿Qué tipo de radio quieres?, Elige 1 para AM o 2 para FM");
+		tipo = scanner.nextInt();*/
+		
+		do {
+			System.out.println("1. Subir frecuencia");
+			System.out.println("2. Bajar frecuencia");
+			System.out.println("3. Mostrar frecuencia");
+			System.out.println("4. Salir");
+			System.out.println("Ingrese una opcion");
+			
+			switch(tipo) {
+			case 1:
+				radioFM.aumentarFrecuencia(frecuenciaInicial, tipo);
+			break;
+			case 2:
+				radioFM.reducirFrecuencia(frecuenciaInicial, tipo);
+			break;
+			case 3: System.out.println("Frecuencia actual: "+radioFM.getFrecuencia()+"FM");
+			break;
+			case 4:
+				System.out.println("Saliendo del programa");
+			break;
+			default:
+				System.out.println("Opcion invalida. Intentelo de nuevo.");
+			
+			}
+		}while (tipo!=4);
+		
+		
+	}
+
+
+}
---
+++ radio/src/main/java/RadioBorja/Radio/Radio.java
@@ -0,0 +1,55 @@
+package Radio;
+
+import java.util.Scanner;
+
+public class Radio {
+	Scanner scanner = new Scanner (System.in);
+	private final static int frecuenciaMin = 80;
+	private final static int frecuenciaMax = 108;
+	private double frecuencia;
+	private final double variacion = 0.5;
+	
+	public void setFrecuencia(double frecuencia) {
+		while (frecuencia<frecuenciaMin || frecuencia>frecuenciaMax) {
+			System.out.println("Error: frecuencia fuera del rango. Intentalo de nuevo.");
+			System.out.println("Ingrese la frecuencia");
+			frecuencia = scanner.nextDouble();
+		}
+		this.frecuencia = frecuencia;
+	}
+	
+	
+	//Get & Set
+	public double getFrecuencia() {
+		return frecuencia;
+	}
+	
+	public void setFrecuencia() {
+		this.frecuencia = frecuencia;
+	}
+	
+	public static int getFrecuenciaMin() {
+		return frecuenciaMin;
+	}
+	
+	public static int getFrecuenciaMax() {
+		return frecuenciaMax;
+	}
+	
+	//Metodos propios
+	public double aumentarFrecuencia(double frecuencia, double variacion) {
+		frecuencia = frecuencia + variacion;
+		return frecuencia;
+	}
+	
+	public double reducirFrecuencia(double frecuencia, double variacion) {
+		frecuencia = frecuencia - variacion;
+		return frecuencia;
+	}
+	
+	public void mostrarFrecuencia() {
+		System.out.println("La frecuencia es: "+frecuencia);
+	}
+	
+
+}
