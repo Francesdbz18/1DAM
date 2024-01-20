5732b90088501fcc3944b5ec1217169581074252
Francesco <francesdbz18pc@outlook.es>
2024-01-20 12:05:37 +0100

    ae

--- tarearadio/src/main/java/radiofm/RadioFM.java
+++ tarearadio/src/main/java/radiofm/RadioFM.java
@@ -4,6 +4,9 @@ public class RadioFM {
 	private double frecuencia;
     private String nombre;
 
+    public RadioFM () {
+
+    }
     public RadioFM (String nombre, double frecuencia) {
         this.nombre = nombre;
         this.frecuencia = frecuencia;
@@ -24,12 +27,12 @@ public class RadioFM {
     }
 
     /* EMISORAS:
-     * - RADIO MARCA: 103.5 FM
      * - HIT FM: 89.9 FM
      * - CADENA DIAL: 91.7 FM
+     * - LOS40 DANCE: 92.4 FM
+     * - RADIO MARCA: 103.5 FM
      * - BOM RADIO: 105.1 FM
      * - COPE: 106.3 FM
-     * - LOS40 DANCE: 92.4 FM
      */
 
      // Robao: Radio.inicio=(Radio.inicio+1+radio.length)% radio.length; System.out.println("Emisora: "+radio[Radio.inicio].getNombre()+" su frecuencia es: "+radio[Radio.inicio].getFrecuencia());
