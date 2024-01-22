5b9a35035de3e17354de2fccb7af5a9e0cf2e46f
Francesco <francesdbz18pc@outlook.es>
2024-01-22 11:07:40 +0100

    Herencia kkkk

--- herencia/src/main/java/herencia/Main.java
+++ herencia/src/main/java/herencia/Main.java
@@ -1,8 +1,14 @@
 package herencia;
 
+import herencia.Profesor.especialidad;
+
 public class Main {
     public static void main(String[] args) {
         Alumno borjita = new Alumno("Borja", "777", 21, 69420);
         System.out.println(borjita.nombre+" "+borjita.apellido+", "+borjita.edad+", "+ borjita.getNIA());
+
+        Profesor juanito = new Profesor("Juanito", "Alimaña", 68, especialidad.INFORMATICA);
+
+        System.out.println(juanito.toString());
     }
 }
